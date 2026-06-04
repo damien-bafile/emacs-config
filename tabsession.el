a035cc5bed84a3508fafd087a43b20dbe4a1f443
Damien Bafile <bafile.damien@gmail.com>
2026-06-04 19:21:55 +0800

    more betta version

---
+++ tabsession.el
@@ -0,0 +1,13 @@
+;; -*- mode: emacs-lisp; lexical-binding:t; coding: utf-8-emacs; -*-
+;; -------------------------------------------------------------------------
+;; Tabspaces Session File for Emacs
+;; -------------------------------------------------------------------------
+;; Created Mon Jun  1 01:38:14 2026
+
+;; Non-project tabs only (project tabs saved separately)
+
+;; Project to tab name mapping:
+(setq tabspaces-project-tab-map 'nil)
+
+;; Tabs and buffers:
+(setq tabspaces--session-list '((("/home/daimyo/Dev/Worl/.editorconfig") ".editorconfig" (((min-height . 4) (min-width . 10) (min-height-ignore . 3) (min-width-ignore . 4) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 80) (min-pixel-width . 100) (min-pixel-height-ignore . 60) (min-pixel-width-ignore . 40) (min-pixel-height-safe . 20) (min-pixel-width-safe . 20)) leaf (pixel-width . 3416) (pixel-height . 1350) (total-width . 341) (total-height . 67) (normal-height . 1.0) (normal-width . 1.0) (parameters (context)) (buffer ".editorconfig" (selected . t) (hscroll . 0) (fringes 8 8 nil nil) (margins nil) (scroll-bars nil 0 t nil 0 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1)) (prev-buffers ("*dashboard*" 1 643) ("*scratch*" 1 148)))) (("/home/daimyo/Dev/Worl/.editorconfig") "*Messages*" (((min-height . 4) (min-width . 10) (min-height-ignore . 3) (min-width-ignore . 4) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 80) (min-pixel-width . 100) (min-pixel-height-ignore . 60) (min-pixel-width-ignore . 40) (min-pixel-height-safe . 20) (min-pixel-width-safe . 20)) leaf (pixel-width . 3416) (pixel-height . 1350) (total-width . 342) (total-height . 67) (normal-height . 1.0) (normal-width . 1.0) (buffer "*Messages*" (selected . t) (hscroll . 0) (fringes 8 8 nil nil) (margins nil) (scroll-bars nil 0 t nil 0 t nil) (vscroll . 0) (dedicated) (point . 718) (start . 1)) (prev-buffers (".editorconfig" 1 13))))))
