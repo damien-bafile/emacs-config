a119aeeb2bbc1d7fe64acb99708bf59637e8e8da
Damien Bafile <51780613+damien-bafile@users.noreply.github.com>
2026-06-22 12:39:57 +0800

    Add my/project-build, wire SPC p b to it, tangling config.org

--- tabsession.el
+++ tabsession.el
@@ -2,7 +2,7 @@
 ;; -------------------------------------------------------------------------
 ;; Tabspaces Session File for Emacs
 ;; -------------------------------------------------------------------------
-;; Created Mon Jun 15 23:00:53 2026
+;; Created Tue Jun 16 20:54:53 2026
 
 ;; Non-project tabs only (project tabs saved separately)
 
@@ -10,4 +10,4 @@
 (setq tabspaces-project-tab-map 'nil)
 
 ;; Tabs and buffers:
-(setq tabspaces--session-list '((nil "*scratch*" (((min-height . 8) (min-width . 10) (min-height-ignore . 4) (min-width-ignore . 2) (min-height-safe . 2) (min-width-safe . 2) (min-pixel-height . 8) (min-pixel-width . 10) (min-pixel-height-ignore . 4) (min-pixel-width-ignore . 2) (min-pixel-height-safe . 2) (min-pixel-width-safe . 2)) vc (pixel-width . 80) (pixel-height . 24) (total-width . 80) (total-height . 24) (normal-height . 1.0) (normal-width . 1.0) (combination-limit) (leaf (pixel-width . 80) (pixel-height . 19) (total-width . 80) (total-height . 19) (normal-height . 0.7916666666666667) (normal-width . 1.0) (buffer "*scratch*" (selected . t) (hscroll . 0) (fringes 0 0 nil nil) (margins nil) (scroll-bars nil 0 t nil 0 t nil) (vscroll . 0) (dedicated) (point . 148) (start . 1))) (leaf (last . t) (pixel-width . 80) (pixel-height . 5) (total-width . 80) (total-height . 5) (normal-height . 0.20833333333333331) (normal-width . 1.0) (buffer "*Warnings*" (selected) (hscroll . 0) (fringes 0 0 nil nil) (margins nil) (scroll-bars nil 0 t nil 0 t nil) (vscroll . 0) (dedicated) (point . 107) (start . 1)))))))
+(setq tabspaces--session-list '((("/Users/daimyo/.config/emacs/config.org") "*dashboard*" (((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 2) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 4) (min-pixel-width . 10) (min-pixel-height-ignore . 2) (min-pixel-width-ignore . 2) (min-pixel-height-safe . 1) (min-pixel-width-safe . 2)) leaf (pixel-width . 80) (pixel-height . 23) (total-width . 80) (total-height . 23) (normal-height . 1.0) (normal-width . 1.0) (buffer "*dashboard*" (selected) (hscroll . 0) (fringes 0 0 nil nil) (margins nil) (scroll-bars nil 0 t nil 0 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1)) (prev-buffers ("config.org" 6247 43647) ("*dashboard*" 1 1) ("*scratch*" 1 148))))))
